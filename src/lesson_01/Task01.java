309ddd62571a0e5e773b7908e83b4c8088675b88
JimITec <kdsart@mail.ru>
2024-09-15 19:17:14 +0300

    ПО сути

--- src/lesson_01/Task01.java
+++
@@ -1,7 +0,0 @@
-package lesson_01;
-/**Задание такое-то*/
-public class Task01 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/lesson_02/Task01.java
+++
@@ -1,4 +0,0 @@
-package lesson_02;
-
-public class Task01 {
-}
